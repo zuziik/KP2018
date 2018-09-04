4cba35fc910f9f58f9ac3bcb84008d8f4facf38c
Matthijs Jansen <redplanet00@gmail.com>
2018-09-04 15:50:15 +0200

    temp fix

---
+++ kern/main.c
@@ -0,0 +1,74 @@
+#include <kern/console.h>
+#include <kern/monitor.h>
+#include <kern/pmap.h>
+
+#include <inc/boot.h>
+#include <inc/stdio.h>
+#include <inc/string.h>
+
+void kmain(struct boot_info *boot_info)
+{
+    extern char edata[], end[];
+
+    /* Before doing anything else, complete the ELF loading process.
+     * Clear the uninitialized global data (BSS) section of our program.
+     * This ensures that all static/global variables start out zero. */
+    memset(edata, 0, end - edata);
+
+    /* Initialize the console.
+     * Can't call cprintf until after we do this! */
+    cons_init();
+
+    /* Lab 1 memory management initialization functions */
+    mem_init(boot_info);
+
+    /* Drop into the kernel monitor. */
+    while (1)
+        monitor(NULL);
+}
+
+/*
+ * Variable panicstr contains argument to first call to panic; used as flag
+ * to indicate that the kernel has already called panic.
+ */
+const char *panicstr;
+
+/*
+ * Panic is called on unresolvable fatal errors.
+ * It prints "panic: mesg", and then enters the kernel monitor.
+ */
+void _panic(const char *file, int line, const char *fmt,...)
+{
+    va_list ap;
+
+    if (panicstr)
+        goto dead;
+    panicstr = fmt;
+
+    /* Be extra sure that the machine is in as reasonable state */
+    __asm __volatile("cli; cld");
+
+    va_start(ap, fmt);
+    cprintf("kernel panic at %s:%d: ", file, line);
+    vcprintf(fmt, ap);
+    cprintf("\n");
+    va_end(ap);
+
+dead:
+    /* break into the kernel monitor */
+    while (1)
+        monitor(NULL);
+}
+
+/* Like panic, but don't. */
+void _warn(const char *file, int line, const char *fmt,...)
+{
+    va_list ap;
+
+    va_start(ap, fmt);
+    cprintf("kernel warning at %s:%d: ", file, line);
+    vcprintf(fmt, ap);
+    cprintf("\n");
+    va_end(ap);
+}
+
